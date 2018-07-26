1c6bbe495b1418ae21fcc8536db1555b3377c8d8
Paul Campbell <nimbleltd@gmail.com>
2018-07-26 10:48:48 -0500

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My blogpost #{blog}",
+    body: "Integer non sem non magna tempus vestibulum et nec ex. Etiam erat purus, sagittis nec volutpat id, interdum rutrum arcu. Donec sem elit, interdum quis sodales eu, feugiat sed risus. Integer eget ipsum leo. Pellentesque rhoncus mollis ante, sed tincidunt ipsum lacinia eu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In luctus quam ac lacus semper interdum. Donec sed ipsum et risus cursus accumsan. Vivamus tristique ut enim aliquet ultrices. Praesent sed enim libero. Donec rutrum, lectus non pretium elementum, massa ligula ultricies sem, at venenatis libero dolor sed metus."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "rails #{skill}",
+    percent_utilized: "#{skill}"
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "subtitle",
+    body: "body",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/50x100"
+  )
+end
+
+puts "9 portfolio items created"
