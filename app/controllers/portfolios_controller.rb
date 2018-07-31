96e451bbe4890c8dce3bd5e56145a4f4d62a79c4
Paul Campbell <nimbleltd@gmail.com>
2018-07-31 08:44:53 -0500

    integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,24 @@
 class PortfoliosController < ApplicationController
   def index
-      @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Latest post is live.' }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
 end
